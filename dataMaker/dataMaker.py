a7d6981a11d6a7d3ef20863dfd5dc2eb3e413681
Michael D. Hoyle <mhoyle@uoguelph.ca>
2011-11-08 13:32:00 -0500

    Added first feature function.

--- dataMaker/dataMaker.py
+++ dataMaker/dataMaker.py
@@ -1,12 +1,48 @@
+import string
 import sys
+
+honourific_list = [
+	"mr.",
+	"mrs.",
+	"dr.",
+	"ms.",
+	"prof.",
+	"pvt.",
+	"cpl.",
+	"mcpl.",
+	"sgt.",
+	"wo.",
+	"cdt.",
+	"lt.",
+	"cpt.",
+	"mjr.",
+	"col.",
+	"gen."
+]
+
+def isHonourific(context, response):
+	candidate = context[3]
+	for honourific in honourific_list:
+		if candidate.lower == honourific.lower:
+			return true
+
+	return false
 
 if len(sys.argv) == 2:
 	f = open(sys.argv[1], 'r')
 
-	contexts = f.readlines()
+	lines = f.readlines()
+	
+	contexts = []
 
-	for line in contexts:
-		print line
+	for line in lines:
+		words = (string.split(line))
+		for word in words:
+			print word + " ",
+		print
+		contexts.append(words)
+
+	
 
 else:
 	print "Usage: python dataMaker <filename>"
