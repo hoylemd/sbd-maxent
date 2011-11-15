b79037d36bbcacdf50925e6409d3248545e27bdd
Michael D. Hoyle <mhoyle@uoguelph.ca>
2011-11-15 14:21:34 -0500

    Updated the todo file. Added some unit tests.

--- implementation/features.py
+++ implementation/features.py
@@ -33,8 +33,109 @@ def isHonourific(context, response):
 	# fall-through
 	return 0
 
+# feature function for surrounding quotes
+def inQuote(context, response):
+
+	# initialize the quote level
+	quoteLevel = 0
+
+	# calculate the quote level of the prefix
+	for candidate in context[:3]:
+		# print "candidate: '" + candidate + "' 0:'" + candidate[0] + "' -1:'" + candidate[-1] + "'"
+		if candidate[0] == '"':
+			quoteLevel += 1
+		if candidate[-1] == '"':
+			quoteLevel -= 1
+
+	# if we have unbalanced open quotes, we are in a quote
+	if quoteLevel > 0:
+		return 1
+
+	# calculate the quote level of the suffix
+	for candidate in context[4:]:
+		if candidate[0] == '"':
+			quoteLevel += 1
+		if candidate[-1] == '"':
+			quoteLevel -= 1
+
+	# if we have unbalanced closed quotes, we are in a quote
+	if quoteLevel < 0:
+		return 1
+
+	# no quotey stuff count. 
+	return 0
+
 def testFeatures(context):
 	features = dict(
-		honourificTest = isHonourific(context, True)
+		honourificTest = isHonourific(context, True),
+		quoteTest = inQuote(context, True)
 		)
 	return features
+
+# Test them!
+
+def assertVal(testName, condition, result):
+	if condition == result:
+		print testName + " test passed."
+	else :
+		print testName + " test FAILED!"
+
+# test Honourific
+
+# regular, negative
+testName = "honourific negative"
+context = ["this", "is", "not", "an", "honorific", "my", "friend"]
+print "Test case: ",
+print context
+assertVal(testName, isHonourific(context, True), 0);
+print
+
+# regular, positive
+testName = "honourific positive"
+context = ["this", "is", "not", "Mr.", "honorific", "my", "friend"]
+print "Test case: ",
+print context
+assertVal(testName, isHonourific(context, True), 1);
+print
+
+# test inQuote
+
+# regular, negative
+testName = "in quote negative"
+context = ["this", "is", "not", "an", "honorific", "my", "friend"]
+print "Test case: ",
+print context
+assertVal(testName, inQuote(context, True), 0);
+print
+
+# regular, positive, prefix
+testName = "in quote positive prefix"
+context = ["this", "is", "\"not", "Mr.", "honorific", "my", "friend"]
+print "Test case: ",
+print context
+assertVal(testName, inQuote(context, True), 1);
+print
+
+# regular, positive, suffix
+testName = "in quote positive suffix"
+context = ["this", "is", "not", "Mr.", "honorific\"", "my", "friend"]
+print "Test case: ",
+print context
+assertVal(testName, inQuote(context, True), 1);
+print
+
+# regular, negative, prefix quoted word
+testName = "in quote negative prefix quoted word"
+context = ["this", "is", "\"not\"", "Mr.", "honorific", "my", "friend"]
+print "Test case: ",
+print context
+assertVal(testName, inQuote(context, True), 0);
+print
+
+# regular, negative, suffix quoted word
+testName = "in quote negative suffix quoted word"
+context = ["this", "is", "not", "Mr.", "\"honorific\"", "my", "friend"]
+print "Test case: ",
+print context
+assertVal(testName, inQuote(context, True), 0);
+print
